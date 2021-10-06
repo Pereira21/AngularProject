e4a0960487572d6f10b0b6d276e489ac98cef928
Lucas D'Amico <lucas21_pereira@hotmail.com>
2021-10-06 07:06:28 -0300

    navegação imperativa com router api

--- angular-project/src/app/pessoa/edicao/edit-pessoa/edit-pessoa.component.ts
+++ angular-project/src/app/pessoa/edicao/edit-pessoa/edit-pessoa.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Pessoa } from '../../model/pessoa';
 import { PessoaService } from '../../services/pessoa-service.component';
 
@@ -11,15 +11,24 @@ import { PessoaService } from '../../services/pessoa-service.component';
 export class EditPessoaComponent implements OnInit {
 
   pessoa?: Pessoa;
-  constructor(private route: ActivatedRoute, private pessoaService: PessoaService){ }
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private pessoaService: PessoaService){ }
 
   ngOnInit(): void {
     this.route.params
       .subscribe(params => {
         this.pessoa = this.pessoaService.obterPeloId(params['id']);
-
+        
         console.log(this.pessoa);
       })
+  }
+
+  salvar(){
+    //comunicação com backend;
+
+    this.router.navigate(["pessoa/"]);
   }
 
 }
